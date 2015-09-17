bc4bf1ea14619692226a9a99b4850215b7bb4574
dyunkim <dykim@smu.edu>
2015-09-17 11:50:07 -0500

    Creates 'Wallpapers' if necessary; Populates when it exists

--- EarthPornPull.py
+++ EarthPornPull.py
@@ -1,5 +1,6 @@
 import praw
 import urllib
+import os
 
 r = praw.Reddit(user_agent='EarthPornPull')
 topTen = r.get_subreddit('EarthPorn').get_hot(limit=10)
@@ -7,11 +8,15 @@ urlList = []
 for x in topTen:
     urlList.append(x.url)
 count = 0
+if os.path.isdir("Wallpapers") == False:
+    os.makedirs("Wallpapers")
 for x in urlList:
     count += 1
+    imgName = str(count) + ".jpg"
     if x.endswith('.jpg'):
-        urllib.urlretrieve(x, str(count) + ".jpg")
+        urllib.urlretrieve(x, imgName)
     else:
-        urllib.urlretrieve(x+".jpg", str(count) + ".jpg")
+        urllib.urlretrieve(x+".jpg", imgName)
+    os.rename(imgName, "Wallpapers/" + imgName)
 
 print "Done"
